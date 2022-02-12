88d154fe09c8e48dedf867be32362148591e796b
szexian96 <58319901+szexian96@users.noreply.github.com>
2022-02-12 22:31:38 +0800

    upload document

---
+++ crypto-tracker/src/components/CoinsTable.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { useState } from 'react';
+
+const CoinsTable = () => {
+  const [coins, setCoins] = useState([]);
+    const [loading, setLoading] = useState(false);
+  
+  
+  return (
+    <div>CoinsTable</div>
+  )
+}
+
+export default CoinsTable
