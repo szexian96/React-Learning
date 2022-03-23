0141ac17c54c05fcb4b07db36f3cdcd5fdaf061d
szexian96 <szexian96@gmail.com>
2022-03-23 20:03:20 +0900

    update coin

--- crypto-tracker/src/Pages/CoinPage.js
+++ crypto-tracker/src/Pages/CoinPage.js
@@ -1,9 +1,41 @@
-import React from 'react'
+import { makeStyles } from '@material-ui/core';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { SingleCoin } from '../config/api';
+import { CryptoState } from '../CryptoContext';
 
-const CoinPage = () => {
+const CoinPage = () => { 
+  const { id } = useParams();//will only get id from the app.js which is the link id
+  const [coin,setCoin] = useState();
+
+  const {currency,symbol} = CryptoState(); //destruct currency and symbol
+
+  const fetchCoin = async() => {
+    const { data } = await axios.get(SingleCoin(id));
+    setCoin(data); //set data into coin
+  };
+
+  console.log(coin);
+
+  useEffect(() => {
+    fetchCoin();
+  },[]);
+
+  const useStyles = makeStyles(() => ({
+  }));
+
+  const classes = useStyles();
+
   return (
-    <div>CoinPage</div>
-  )
+    <div className = {classes.container}>
+      <div className = {classes.sidebar}>
+        {/*sidebar*/}
+      </div>
+      {/*Chart*/}
+      <CoinInfo coin = {coin} />
+    </div>
+    )
 }
 
 export default CoinPage
--- crypto-tracker/src/config/api.js
+++ crypto-tracker/src/config/api.js
@@ -2,7 +2,7 @@
 export const CoinList = (currency) =>
   `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=100&page=1&sparkline=false`;
 
-export const SingleCoin = (id) =>
+export const SingleCoin = (id) => //get the id and we will get all the things
   `https://api.coingecko.com/api/v3/coins/${id}`;
 
 export const HistoricalChart = (id, days = 365, currency) =>
